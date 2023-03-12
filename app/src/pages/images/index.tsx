53c726d4182e4b3a18f9fd3c3f5d126d979b646e
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2023-03-12 18:29:12 +0200

    Improve spacing

--- app/src/pages/images/index.tsx
+++ app/src/pages/images/index.tsx
@@ -35,10 +35,10 @@ const ImagesPage = () => {
           An error occurred while fetching the images. Please, reload the page!
         </p>
       )}
-      <div className="grid items-end justify-items-center gap-y-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="mt-20 grid items-end justify-items-center gap-y-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {images.map((image) => (
           <Link href={`/images/${image.id}`} key={image.id}>
-            <h2 className="my-3 text-lg font-medium tracking-tight text-gray-200 sm:text-xl">
+            <h2 className="mb-4 text-lg font-medium tracking-tight text-gray-200 sm:text-xl">
               {image.title}
             </h2>
             <ImageComponent
@@ -52,7 +52,7 @@ const ImagesPage = () => {
         {imagesQuery.isFetching &&
           Array.from({ length: PAGE_SIZE }, (_, index) => (
             <div key={index} className="animate-pulse blur">
-              <h2 className="my-3 text-lg font-medium tracking-tight text-gray-200 sm:text-xl">
+              <h2 className="mb-4 text-lg font-medium tracking-tight text-gray-200 sm:text-xl">
                 Placeholder
               </h2>
               <ImagePlaceholder width={256} height={256} />
@@ -64,7 +64,7 @@ const ImagesPage = () => {
           disabled={imagesQuery.isFetching}
           onClick={() => imagesQuery.fetchNextPage()}
           className={classNames(
-            "relative mt-10 overflow-hidden rounded-md px-3.5 py-2.5 font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400",
+            "relative mt-20 overflow-hidden rounded-md px-3.5 py-2.5 font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400",
             !imagesQuery.isFetching
               ? "bg-indigo-600 hover:bg-indigo-500"
               : "bg-gray-800"
